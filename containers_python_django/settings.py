97ba0521dfa61227e4c1846f16594da9036ed506
Rurik D'Angelo <rurik@back4app.com>
2024-09-25 10:34:47 -0300

    Update settings.py

--- containers_python_django/settings.py
+++ containers_python_django/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-m+1z_2#1!&$$*72a%&z3j4!a=7^=!5@stt)-&v+9ray#1u+csa
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['node254a.containers.back4app.com', 'node256a.containers.back4app.com','node255a.containers.back4app.com']
+ALLOWED_HOSTS = ['node254a.containers.back4app.com','node256a.containers.back4app.com','node255a.containers.back4app.com','djangotest-plgaaj8k.b4a.run']
 
 
 # Application definition
